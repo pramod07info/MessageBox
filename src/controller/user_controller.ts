c2b2eabaa64b648fc69b7c76bac5fa29b570c18e
pramod07info <pramod07info@gmail.com>
2020-09-22 23:59:59 +0530

    GET TYPE API DONE

--- src/controller/user_controller.ts
+++ src/controller/user_controller.ts
@@ -9,6 +9,9 @@ class UserController {
     public path = '/user';
     public pathCompose = '/compose';
     public pathMessage = '/message';
+    public pathConversationUserName = '/conversation/:userName';
+    public pathConversationUserNameAndRecipientUserName = '/conversation/:userName/:recipientUserName';
+    public pathRecipient = '/recipient/:userName';
     public router = express.Router();
     public app = express();
 
@@ -19,6 +22,9 @@ class UserController {
         this.router.post(this.path,cors(), this.createUser);
         this.router.post(this.pathCompose,cors(), this.composeMessage);
         this.router.post(this.pathMessage,cors(), this.createMessage);
+        this.router.get(this.pathConversationUserName,cors(), this.getConversationMessage);
+        this.router.get(this.pathConversationUserNameAndRecipientUserName,cors(), this.getConversatonUsierNameAndRecipientUserName);
+        this.router.get(this.pathRecipient,cors(), this.getRecipient);
     }
     createUser = async (request: express.Request, response: express.Response) => {
         LoggerService.writeInfoLog("============ Request For Create Feed back==============="+request.body);
@@ -33,6 +39,21 @@ class UserController {
     createMessage = async (request: express.Request, response: express.Response) => {
         LoggerService.writeInfoLog("============ Request For Create Feed back==============="+request.body);
         const result = await this.userRepository.createMessage(request)
+        response.send(result);
+    }
+    getConversationMessage = async (request: express.Request, response: express.Response) => {
+        LoggerService.writeInfoLog("============ Request For Create Feed back==============="+request.body);
+        const result = await this.userRepository.getConversationByUserName(request)
+        response.send(result);
+    }
+    getConversatonUsierNameAndRecipientUserName = async (request: express.Request, response: express.Response) => {
+        LoggerService.writeInfoLog("============ Request For Create Feed back==============="+request.body);
+        const result = await this.userRepository.getConversationByUserNameAndRecepientUserName(request)
+        response.send(result);
+    }
+    getRecipient = async (request: express.Request, response: express.Response) => {
+        LoggerService.writeInfoLog("============ Request For Create Feed back==============="+request.body);
+        const result = await this.userRepository.getConversationByRecepientUserName(request)
         response.send(result);
     }
 }
---
+++ src/dto/conversation.ts
@@ -0,0 +1,7 @@
+export interface Conversation{
+    conversationId: number;
+    recipientName: any;
+    recipientPicture: any;
+    recipientUserName: any;
+    messages:any;
+}
