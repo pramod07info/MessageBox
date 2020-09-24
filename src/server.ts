edcab8d4971d40f51e73500a156f7c46c39c4131
TUSHAR KHARAT <kharat.tushar11@gmail.com>
2020-09-24 16:47:50 +0530

    Changes made that are required to deploy to heroku

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,12 @@
 import App from './app';
 import UserController from './controller/user_controller';
 
+const PORT : number = Number(process.env.PORT) || 8888;
 const app = new App(
   [
     new UserController()
   ],
-  8888,
+  PORT,
 );
  
 app.listen();
